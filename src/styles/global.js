105f50ccb7dd73ae6659604ece837601129ecc01
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-15 00:15:46 -0300

    customize spectre.css

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
-import "spectre.css/dist/spectre.min.css";
-import "spectre.css/dist/spectre-exp.min.css";
-import "spectre.css/dist/spectre-icons.min.css";
+
+import './spectre.scss'
 
 const GlobalStyle = createGlobalStyle`
   *{
