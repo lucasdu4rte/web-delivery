85570cffcbfad36f96aa7afad1bb6bdcdee9cd8a
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-29 03:40:10 -0300

    Formated currency

--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -2,36 +2,46 @@ import React, { useState, useEffect } from "react";
 import NavBar from "components/NavBar";
 import api from "api";
 // import pizza from 'images/Pizzas/1.png'
-import moment from "moment"
-import 'moment/locale/pt-br'
+import moment from "moment";
+import "moment/locale/pt-br";
 
-moment.locale('pt-br')
+moment.locale("pt-br");
 
 const Orders = ({ user, token }) => {
-  const [orders, setOrders] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [orders, setOrders] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    api.get('/orders').then(({data}) => {
-      setOrders(data.sort((a, b) => moment(b.order_date) - moment(a.order_date)))
-      setLoading(false)
-    })
-  }, [])
+    api.get("/orders").then(({ data }) => {
+      setOrders(
+        data
+          .map(order => ({
+            ...order,
+            totalFormated: new Intl.NumberFormat("pt-br", {
+              style: "currency",
+              currency: "BRL"
+            }).format(order.total),
+            orderDateFormated: moment(order.order_date).fromNow()
+          }))
+          .sort((a, b) => moment(b.order_date) - moment(a.order_date))
+      );
+      setLoading(false);
+    });
+  }, []);
 
-  console.log('orders', orders)
+  console.log("orders", orders);
   return (
     <>
       <NavBar />
-      <div className="container">
+      <div className="container mb-2">
         <div className="columns">
           <div className="column col-8 col-mx-auto p-2 mt-2">
             <strong>Últimos pedidos</strong>
-            <br/>
+            <br />
 
-            {loading && 'Carregando...'}
+            {loading && "Carregando..."}
 
-            {
-              orders.map(order =>
+            {orders.map(order => (
               <div
                 key={order.id}
                 className="card mt-2"
@@ -45,34 +55,49 @@ const Orders = ({ user, token }) => {
                     Pedido <strong>#{order.id}</strong> - {order.user.name}
                   </div>
                   {/* há 2 segundos */}
-                  <small className="card-subtitle text-gray">{moment(order.order_date).fromNow()}</small> <br />
-                  <strong className="card-subtitle text-dark">R${order.total}</strong>
+                  <small className="card-subtitle text-gray">
+                    {order.orderDateFormated}
+                  </small>{" "}
+                  <br />
+                  <strong className="card-subtitle text-dark">
+                    {order.totalFormated}
+                  </strong>
                 </div>
 
                 <div className="card-body">
-                  <div className="divider"></div>
+                  <div className="divider" />
                   <div className="columns">
-                    {
-                      order.products.map(product =>
+                    {order.products.map(product => (
                       <div key={product.id} className="column col-6">
-                        <div className="tile p-2" style={{ border: '1px solid #f1f2f6' }}>
+                        <div
+                          className="tile p-2"
+                          style={{ border: "1px solid #f1f2f6" }}
+                        >
                           <div className="tile-icon">
                             <div className="">
                               {/* <i className="icon icon-file centered" /> */}
-                              <img className="centered" src={product.photo_url} alt={product.description} width={60} />
+                              <img
+                                className="centered"
+                                src={product.photo_url}
+                                alt={product.description}
+                                width={60}
+                              />
                             </div>
                           </div>
                           <div className="tile-content">
-                            <div className="tile-title">{product.description}</div>
-                            <small className="tile-subtitle">Tamanho: Média</small>
+                            <div className="tile-title">
+                              {product.description}
+                            </div>
+                            <small className="tile-subtitle">
+                              Tamanho: Média
+                            </small>
                           </div>
                         </div>
                       </div>
-                      )
-                    }
+                    ))}
                   </div>
 
-                  <div className="divider"></div>
+                  <div className="divider" />
                 </div>
 
                 <div className="card-footer">
@@ -80,10 +105,7 @@ const Orders = ({ user, token }) => {
                   {order.observation}
                 </div>
               </div>
-
-              )
-            }
-
+            ))}
           </div>
         </div>
       </div>
