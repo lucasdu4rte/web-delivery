935c0625e8fd9797cbfe1bb57ea3698368b49fbb
Lucas Duarte <lucasdu4rte@gmail.com>
2019-06-29 12:24:34 -0300

    Padding in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,10 +2,18 @@ import React from "react";
 // import logo from "images/logo.png";
 const NavBar = () => {
   return (
-    <header className="navbar px-2 py-2 " style={{ background: "#0b2031" }}>
+    <header
+      className="navbar"
+      style={{
+        background: "#0b2031",
+        paddingLeft: "10%",
+        paddingRight: "10%",
+        height: "80px"
+      }}
+    >
       <section className="navbar-section">
         <a href="..." className="navbar-brand mr-2">
-          <img src={process.env.PUBLIC_URL + 'assets/images/logo.png'} alt="" />
+          <img src={process.env.PUBLIC_URL + "assets/images/logo.png"} alt="" />
         </a>
         <h5 className="text-light">Pizzaria Don Juan</h5>
       </section>
@@ -22,12 +30,12 @@ const NavBar = () => {
             </div>
           </div>
           <div className="divider-vert" />
-          <div className="column">
-            <button className="btn btn-primary btn-action s-circle">
-              <i className="icon icon-more-vert" />
-            </button>
-          </div>
         </div>
+        <section className="navbar-center" style={{ paddingLeft: "0.4rem" }}>
+          <button className="btn btn-primary btn-action s-circle">
+            <i className="icon icon-menu" />
+          </button>
+        </section>
       </section>
     </header>
   );
