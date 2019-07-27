916852e5cf48b037b9699512c3abae059ad454d0
Lucas Duarte <lucasdu4rte@gmail.com>
2019-07-27 02:33:37 -0300

    reactotron configs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import GlobalStyles from 'styles/global';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
 
 import Routes from 'routes';
 import { AuthProvider } from 'providers/AuthComponent';
 
+import store from 'store';
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyles />
       <AuthProvider>
         <Routes />
       </AuthProvider>
-    </>
+    </Provider>
   );
 }
 
